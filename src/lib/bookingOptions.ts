a0e035ffc427381f41c2bcf3574d51e4fb9a9ea0
marcusliew1-svg <marcusliew1@gmail.com>
2026-08-15 07:57:33 +0700

    Add shared booking option contract

---
+++ src/lib/bookingOptions.ts
@@ -0,0 +1,26 @@
+export const bookingInterests = [
+  "Discovery discussion",
+  "Membership",
+  "Health screening",
+  "Personalised longevity",
+  "Corporate executive wellness",
+  "International medicine access intelligence",
+  "SCF lab roadmap",
+  "Education with Ling",
+] as const;
+
+export const bookingEnquiringFor = [
+  "Myself",
+  "Family member",
+  "Company",
+  "Executive team",
+  "Other",
+] as const;
+
+export const bookingMembershipOptions = [
+  "Not sure yet",
+  "Ascend",
+  "Evolve",
+  "Eterna",
+  "Pinnacle",
+] as const;
