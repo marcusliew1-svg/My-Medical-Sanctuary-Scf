6eefe8af3d7eb58164e49c8f246312eb8cd1c432
marcusliew1-svg <marcusliew1@gmail.com>
2026-08-22 13:23:29 +0700

    Make Malay regional route async-param compatible

--- src/app/ms/[section]/page.tsx
+++ src/app/ms/[section]/page.tsx
@@ -1,7 +1,12 @@
 import { notFound } from "next/navigation";
 import { isRegionalSection, LocalizedRegionalPage } from "@/components/LocalizedRegionalExperience";
 
-export default function MalayRegionalPage({ params }: { params: { section: string } }) {
-  if (!isRegionalSection(params.section)) notFound();
-  return <LocalizedRegionalPage locale="ms" section={params.section} />;
+type MalayRegionalPageProps = {
+  params: Promise<{ section: string }> | { section: string };
+};
+
+export default async function MalayRegionalPage({ params }: MalayRegionalPageProps) {
+  const { section } = await params;
+  if (!isRegionalSection(section)) notFound();
+  return <LocalizedRegionalPage locale="ms" section={section} />;
 }
