9410d002fd35bafca03111f99aae8e23a12dc184
marcusliew1-svg <marcusliew1@gmail.com>
2026-08-22 13:24:12 +0700

    Make Thai regional route async-param compatible

--- src/app/th/[section]/page.tsx
+++ src/app/th/[section]/page.tsx
@@ -1,7 +1,12 @@
 import { notFound } from "next/navigation";
 import { isRegionalSection, LocalizedRegionalPage } from "@/components/LocalizedRegionalExperience";
 
-export default function ThaiRegionalPage({ params }: { params: { section: string } }) {
-  if (!isRegionalSection(params.section)) notFound();
-  return <LocalizedRegionalPage locale="th" section={params.section} />;
+type ThaiRegionalPageProps = {
+  params: Promise<{ section: string }> | { section: string };
+};
+
+export default async function ThaiRegionalPage({ params }: ThaiRegionalPageProps) {
+  const { section } = await params;
+  if (!isRegionalSection(section)) notFound();
+  return <LocalizedRegionalPage locale="th" section={section} />;
 }
