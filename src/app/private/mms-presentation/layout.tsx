a01d2c963145750341f65675649e35880a221623
marcusliew1-svg <marcusliew1@gmail.com>
2026-09-05 14:03:17 +0700

    Load MMS story substance styles

--- src/app/private/mms-presentation/layout.tsx
+++ src/app/private/mms-presentation/layout.tsx
@@ -1,6 +1,7 @@
 import Script from "next/script";
 import "./presentation.css";
 import "./autoplay.css";
+import "./substance.css";
 
 export default function MMSPresentationLayout({ children }: { children: React.ReactNode }) {
   return <>{children}<Script src="/mms-story-substance.js" strategy="afterInteractive" /><Script src="/mms-story-autoplay.js" strategy="afterInteractive" /></>;
