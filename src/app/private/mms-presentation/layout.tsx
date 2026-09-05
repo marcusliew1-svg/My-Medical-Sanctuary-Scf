c184808bf43b2912635a1f833f3b1a0176820127
marcusliew1-svg <marcusliew1@gmail.com>
2026-09-05 13:40:33 +0700

    Load cinematic autoplay engine for MMS story preview

--- src/app/private/mms-presentation/layout.tsx
+++ src/app/private/mms-presentation/layout.tsx
@@ -1,5 +1,7 @@
+import Script from "next/script";
 import "./presentation.css";
+import "./autoplay.css";
 
 export default function MMSPresentationLayout({ children }: { children: React.ReactNode }) {
-  return children;
+  return <>{children}<Script src="/mms-story-autoplay.js" strategy="afterInteractive" /></>;
 }
