d2bb90b45fb28dd7693fd6da25d64823fae5ef68
marcusliew1-svg <marcusliew1@gmail.com>
2026-09-05 14:08:39 +0700

    Load MMS scene-level cinematic motion graphics

--- src/app/private/mms-presentation/layout.tsx
+++ src/app/private/mms-presentation/layout.tsx
@@ -2,7 +2,8 @@ import Script from "next/script";
 import "./presentation.css";
 import "./autoplay.css";
 import "./substance.css";
+import "./cinematics.css";
 
 export default function MMSPresentationLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}<Script src="/mms-story-substance.js" strategy="afterInteractive" /><Script src="/mms-story-autoplay.js" strategy="afterInteractive" /></>;
+  return <>{children}<Script src="/mms-story-substance.js" strategy="afterInteractive" /><Script src="/mms-story-cinematics.js" strategy="afterInteractive" /><Script src="/mms-story-autoplay.js" strategy="afterInteractive" /></>;
 }
