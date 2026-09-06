fff93f831858b8c0ca66cb0af9a61f51f072e69c
marcusliew1-svg <marcusliew1@gmail.com>
2026-09-06 07:09:33 +0700

    Load substantive MMS reading panels and disable placeholder voice

--- src/app/private/mms-presentation/layout.tsx
+++ src/app/private/mms-presentation/layout.tsx
@@ -8,7 +8,8 @@ import "./multishot.css";
 import "./film-polish.css";
 import "./focus.css";
 import "./caption-sync.css";
+import "./reading.css";
 
 export default function MMSPresentationLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}<Script src="/mms-story-substance.js" strategy="afterInteractive" /><Script src="/mms-story-deepdive.js" strategy="afterInteractive" /><Script src="/mms-story-multishot.js" strategy="afterInteractive" /><Script src="/mms-story-cinematics.js" strategy="afterInteractive" /><Script src="/mms-story-film-polish.js" strategy="afterInteractive" /><Script src="/mms-story-focus.js" strategy="afterInteractive" /><Script src="/mms-story-narration.js" strategy="afterInteractive" /><Script src="/mms-story-autoplay.js" strategy="afterInteractive" /><Script src="/mms-story-caption-sync.js" strategy="afterInteractive" /></>;
+  return <>{children}<Script src="/mms-story-substance.js" strategy="afterInteractive" /><Script src="/mms-story-deepdive.js" strategy="afterInteractive" /><Script src="/mms-story-multishot.js" strategy="afterInteractive" /><Script src="/mms-story-cinematics.js" strategy="afterInteractive" /><Script src="/mms-story-film-polish.js" strategy="afterInteractive" /><Script src="/mms-story-focus.js" strategy="afterInteractive" /><Script src="/mms-story-reading.js" strategy="afterInteractive" /><Script src="/mms-story-narration.js" strategy="afterInteractive" /><Script src="/mms-story-autoplay.js" strategy="afterInteractive" /><Script src="/mms-story-caption-sync.js" strategy="afterInteractive" /><Script src="/mms-story-voice-placeholder.js" strategy="afterInteractive" /></>;
 }
